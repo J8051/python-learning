3651945b224d3c9d1f2d057a4abe60b1722ced8e
J8051 <nathaniel.jessica@yahoo.ca>
2023-10-31 21:03:43 -0400

    learn python pt 2

--- main.py
+++ main.py
@@ -21,3 +21,85 @@ print("Principal Amount: $" + str(int(principalAmount)))
 print("Annual Interest Rate: " + str(int(annualInterestRatePercent)) + "%")
 print("Number of Years: " + str(int(numbOfyears)))
 print("Simple Interest Earned: $" + simpleInterest)
+
+#1.The average grade of the students
+
+numOfStudents = input("Enter the number of students: ")
+
+listOfGrades = []
+i = 0
+while len(listOfGrades) < int(numOfStudents):
+  i += 1
+  grade = input("Enter the grade for student " + str(i) + ": ")
+  listOfGrades.append(grade)
+
+total = 0
+for x in listOfGrades:
+  total += int(x)
+
+average = total / len(listOfGrades)
+print("Average Grade: " + str(average))
+
+#2. The highest grade of the students
+
+highest = listOfGrades[0]
+for j in listOfGrades:
+  if j > highest:
+    highest = j
+print("Highest Grade: "+ str(highest))
+
+
+#2. The lowest grade of the students
+
+lowest = listOfGrades[0]
+for y in listOfGrades:
+  if y < lowest:
+    lowest = y
+print("Lowest Grade: " + str(lowest))
+
+
+#3. The number of students who passed
+
+studentsPassed = 0
+for i in listOfGrades:
+  if int(i) >= 60:
+    studentsPassed += 1
+print("Number Of Students Passed: " + str(studentsPassed))
+
+
+#4. The number of students who failed
+
+studentsFailed = 0
+for a in listOfGrades:
+  if int(a) < 60:
+    studentsFailed += 1
+print("Number of Students failed: " + str(studentsFailed))
+
+
+#5. Grade Modification
+
+modifiedGrades = []
+
+for originalGr in listOfGrades:
+  originalGr = int(originalGr)
+  if originalGr < average:
+    originalGr += 5
+  modifiedGrades.append(originalGr)
+print("Modified Grades: " + str(modifiedGrades))
+
+#6. Letter Grades
+
+letterGrades = []
+for modGr in modifiedGrades:
+  modGr = int(modGr)
+  if modGr < 60:
+    letterGrades.append("F")
+  if modGr >= 90:
+    letterGrades.append("A")
+  if modGr >= 60 and modGr <= 69:
+    letterGrades.append("D")
+  if modGr >= 70 and modGr <= 79:
+    letterGrades.append("C")
+  if modGr >= 80 and modGr <= 89:
+    letterGrades.append("B")
+print("Letter Grades: " + str(letterGrades))
